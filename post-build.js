9f43ad9ee4014c4984e88d1acab7558cb63a0dd5
Sam Jones <sam@decisions.com>
2019-05-06 14:03:19 -0400

    copy docs into dist during post-build

--- post-build.js
+++ post-build.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const prettier = require('prettier');
+const ncp = require('ncp');
+
+console.log("Creating package.json");
 
 let package = require("./package.json");
-
 
 let distPackage = Object.assign({}, package);
 delete distPackage.scripts;
@@ -13,8 +15,18 @@ distPackageString = prettier.format(distPackageString, { parser: "json"});
 fs.writeFileSync("./dist/package.json", distPackageString);
 
 /* Copy the README */
+console.log("Copying README");
 
 var readmeString = fs.readFileSync("README.md", "utf8");
 readmeString = readmeString.replace('./src/', './');
 
 fs.writeFileSync("./dist/README.md", readmeString);
+
+console.log("Copying docs");
+
+ncp('./docs', './dist/docs', function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Build Complete.');
+ });
